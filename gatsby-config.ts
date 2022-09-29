eddf462e83f65ad36ce144cfb4ba68ebcceda3c8
Brooks <alienization135@gmail.com>
2022-09-29 15:21:50 -0500

    sumo

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,21 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `splashpage`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.blockheads.work`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-emotion"]
+  plugins: [
+    "gatsby-plugin-emotion", 
+    {
+      resolve : `gatsby-plugin-sumo`,
+      options : {
+        sumoSiteId : `415b46bb2d79c21aadb8f4728ce80e19129b39d936aa28d46de7afaf75066870` //Sumo Site ID
+      }
+    }
+  ]
 };
 
 export default config;
